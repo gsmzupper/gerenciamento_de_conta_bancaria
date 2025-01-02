2de7545058f63ccece3fca113fa09ffacbd96608
Gabriel Costa <gabriel.costa@zup.com.br>
2025-01-02 12:14:24 -0300

    feat(Conta): Adiciona a possibilidade de identificar o tipo de conta com base em um dígito
    Adiciona atributo dígito, para possibilitar a identificação do tipo de conta.
    Adiciona atributo identificacao, para receber a concatenação do número da conta com o dígito.
    Implementa o método getDigito.
    Implementa o método adicionaDigito, para concatenar o número da conta ao dígito.
    Implementa o método getIdentificacao.

--- src/conta/Conta.java
+++ src/conta/Conta.java
@@ -2,9 +2,11 @@ package conta;
 
 public abstract class Conta {
 	private final int NUMEROCONTA;
+	private final int DIGITO;
+	private final String IDENTIFICACAOCONTA;
 	private double saldo;
 
-	public Conta(int NUMEROCONTA) {
+	public Conta(int NUMEROCONTA, int DIGITO) {
 		int limiteMinimo = 10000000;
 		int limiteMaximo = 99999999;
 
@@ -13,6 +15,8 @@ public abstract class Conta {
 		}
 
 		this.NUMEROCONTA = NUMEROCONTA;
+		this.DIGITO = DIGITO;
+		this.IDENTIFICACAOCONTA = null;
 		this.saldo = 0;
 	}
 
@@ -24,12 +28,24 @@ public abstract class Conta {
 		return saldo;
 	}
 
+	public int getDIGITO() {
+		return DIGITO;
+	}
+
+	public String getIDENTIFICACAOCONTA() {
+		return IDENTIFICACAOCONTA;
+	}
+
 	protected void adiciona(double valor) {
 		this.saldo += valor;
 	}
 
 	protected void subtrai(double valor) {
 		this.saldo -= valor;
+	}
+
+	public void  adicionaDigito(){
+		this.IDENTIFICACAOCONTA= getNUMEROCONTA() + "-" + getDIGITO();
 	}
 
 	public abstract void exibeDetalhes();

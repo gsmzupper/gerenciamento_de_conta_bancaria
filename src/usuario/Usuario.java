0a9a26ba945d250dddcde6cba84d3e5e5373e76e
alex junior <alex.gomes@zup.com.be>
2025-01-03 11:10:02 -0300

    primeiro commit

---
+++ src/usuario/Usuario.java
@@ -0,0 +1,18 @@
+    package usuario;{
+    import java.util.ArrayList;
+    import java.util.List;
+// Classe que representa um cliente do banco
+    // Classe que representa um cliente do banco
+    Vclass Cliente {
+        private String nome;
+
+
+
+
+
+
+
+
+
+
+}

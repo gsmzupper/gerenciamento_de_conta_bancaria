7b263c35856ed9ff22ee4c3534b4a6ef85a003d6
gsmzupper <gabriel.merconi@zup.com.br>
2025-01-06 13:32:19 -0300

    feat(GeradorDeContas): implementa correcoes na classe GeradorDeContas

--- src/gerador/GeradorDeContas.java
+++ src/gerador/GeradorDeContas.java
@@ -6,7 +6,7 @@ public class GeradorDeContas {
     private static final Random geradorDeNumero = new Random();
 
     public static int numeroDaConta() {
-        return 100000000 + geradorDeNumero.nextInt(90000000);
+        return 10000000 + geradorDeNumero.nextInt(9999999);
     }
 
     public static int numeroDigito() {
